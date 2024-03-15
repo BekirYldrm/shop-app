0e8e2902ac8eb3d5ba357fe99b6cae83d487d1c6
Fatih-Y <fatihyiimaz@outlook.com>
2024-03-15 15:01:58 +0300

    second commit

--- src/main/java/com/pinsoft/shopapp/business/UserController.java
+++ src/main/java/com/pinsoft/shopapp/business/UserController.java
@@ -3,6 +3,8 @@ package com.pinsoft.shopapp.business;
 import com.pinsoft.shopapp.entity.User;
 import com.pinsoft.shopapp.repository.UserRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,11 +18,24 @@ public class UserController {
 
     @Autowired
     private UserRepository repository;
+    @Operation(tags = "List Users", description = "Get All Users", responses = {
+            @ApiResponse(description = "Success", responseCode = "200"
+
+            ), @ApiResponse(description = "Data Not Found", responseCode = "404"
+
+    ) })
 
     @GetMapping("/getUser")
     public List<User> getAllUsers() {
         return repository.findAll();
     }
+
+    @Operation(tags = "Select Users", description = "Get All Users", responses = {
+            @ApiResponse(description = "Success", responseCode = "200"
+
+            ), @ApiResponse(description = "Data Not Found", responseCode = "404"
+
+    ) })
 
     @GetMapping("/getUserByUsername/{username}")
     public User getUser(@PathVariable String username) {
