552f548b16524846a4fdd595de86ddc9a0ce438f
Bekir <bekiryildirim.2508@gmail.com>
2024-03-17 11:44:06 +0300

    added @Repository annotation.

--- src/main/java/com/pinsoft/shopapp/repository/RoleRepository.java
+++ src/main/java/com/pinsoft/shopapp/repository/RoleRepository.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pinsoft.shopapp.entity.Role;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer>{
 
     List<Role> findByName(String name);
